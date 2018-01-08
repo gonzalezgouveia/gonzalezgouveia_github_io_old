0a45c3e4b435ba56cac558bfdd937c319cd5ba04
Rafa <gonzalez.gouveia@gmail.com>
2018-01-07 19:29:33 -0600

    cambio php

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<?php include('header.php'); ?>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Rafael Gonzalez, Data Scientist</title>
+		<!-- Bootstrap -->
+		<link href="./css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<!-- link to main stylesheet -->
+		<link rel="stylesheet" type="text/css" href="./css/main.css">
+		<nav>
+    		<ul>
+        		<li><a href="./index.html">Home</a></li>
+						<li><a href="./about.html">Sobre mí</a></li>
+	        	<li><a href="./projects.html">Proyectos</a></li>
+        		<li><a href="./documents/CV-ES-RafaelGonzalezGouveia.pdf">CV</a></li>
+        		<!-- <li><a href="./blog.html">Blog</a></li> -->
+						<li><a href="./eng_index.html">English</a></li>
+    		</ul>
+		</nav>
+	</head>
+<body>
+		<div class="container">
+			<img src="./images/rafa_semiformal.jpg" />
+			<h1>¡Hola, soy Rafa!</h1>
+			<hr>
+				<div class="row">
+					<div class="col-sm-6">
+						<h3> ¿Quién soy? </h3>
+						<p ALIGN="justify"> Soy un entusiasta de la ciencia de datos.
+						  Me gustan estudiar matemáticas y conocer nuevas
+							herramientas de programación.</p>
+						<center> <a href="./about.html"> Conoce más de mí...</a> </center>
+					</div>
+					<div class="col-sm-6">
+						<h3> ¿Qué se hacer? </h3>
+						<p align="justify"> Ofrecer soluciones a problemas de negocios mediante
+							 de análisis estadístico y visualización computacional.
+						</p>
+						<center> <a href="./projects.html"> Conoce más de mis proyectos...</a> </center>
+
+	    		</div>
+				</div>
+				<!-- <p></p> -->
+				<br>
+		</div>
+
+		<footer>
+    		<ul>
+        		<li><a href="mailto:gonzalez.gouveia@gmail.com">Email</a></li>
+						<li><a href="https://www.linkedin.com/in/gonzalezgouveia/">LinkedIn</a></li>
+        		<li><a href="https://github.com/gonzalezgouveia">GitHub</a></li>
+			</ul>
+		</footer>
+	</body>
+</html>
